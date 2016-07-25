d72c95435fc2e99412beb8031f8e6052333e2a58
tainahemmanuele <tainahemmanuele@gmail.com>
2016-07-24 22:56:55 -0300

    Algumas edições de html e testes . Falta formatar conforme modelo projeto. -Tainah Emmanuele- 24/07/2016 - 22:56

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,9 +4,10 @@ import play.*;
 import play.mvc.*;
 import play.db.jpa.*;
 import views.html.*;
-import models.Person;
+import models.Usuario;
 import play.data.FormFactory;
 import javax.inject.Inject;
+import java.util.ArrayList;
 import java.util.List;
 
 import static play.libs.Json.*;
@@ -14,22 +15,41 @@ import static play.libs.Json.*;
 public class Application extends Controller {
 
     @Inject
-    FormFactory formFactory;
+    private FormFactory formFactory;
+    private List<Usuario> listaUsuarios = new ArrayList<>();
 
     public Result index() {
-        return ok(index.render());
+        return ok(index.render(listaUsuarios));
     }
 
-    @Transactional
-    public Result addPerson() {
-        Person person = formFactory.form(Person.class).bindFromRequest().get();
-        JPA.em().persist(person);
+
+    public Result addUsuario() {
+        Usuario usuario= formFactory.form(Usuario.class).bindFromRequest().get();
+        listaUsuarios.add(usuario);
+        //JPA.em().persist(usuario);
         return redirect(routes.Application.index());
     }
 
-    @Transactional(readOnly = true)
-    public Result getPersons() {
-        List<Person> persons = (List<Person>) JPA.em().createQuery("select p from Person p").getResultList();
-        return ok(toJson(persons));
+    public Result usuario(String email){
+        return ok(usuario.render(getUsuarioEmail(email)));
+    }
+
+    private Usuario getUsuarioEmail(String email){
+        for(Usuario usuario:listaUsuarios){
+            if(usuario.getEmail().equals(email)){
+                return usuario;
+            }
+
+        }
+        return null;
+    }
+
+
+    public Result editarUsuario(String email)throws Exception{
+        Usuario usuarioAtual= getUsuarioEmail(email);
+        Usuario usuarioNovo = formFactory.form(Usuario.class).bindFromRequest().get();
+        usuarioAtual.setUsername(usuarioNovo.getUsername());
+        usuarioAtual.setEmail(usuarioNovo.getEmail());
+        return redirect(routes.Application.index());
     }
 }
