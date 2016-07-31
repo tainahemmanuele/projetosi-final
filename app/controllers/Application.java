d34a2a6d12f73b56e46029331878a1b893b175a1
tainahemmanuele <tainahemmanuele@gmail.com>
2016-07-30 22:21:30 -0300

    Alterações no html  - Tainah Emmanuele

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -27,11 +27,16 @@ public class Application extends Controller {
         Usuario usuario = formFactory.form(Usuario.class).bindFromRequest().get();
         listaUsuarios.add(usuario);
         //JPA.em().persist(usuario);
-        return redirect(routes.Application.login());
+        return redirect(routes.Application.mensagem());
+
     }
 
     public Result usuario(String email){
         return ok(usuario.render(getUsuarioEmail(email)));
+    }
+
+    public Result mensagem(){
+        return ok(mensagem.render(""));
     }
 
     public Result login(){
@@ -55,4 +60,35 @@ public class Application extends Controller {
         usuarioAtual.setEmail(usuarioNovo.getEmail());
         return redirect(routes.Application.index());
     }
+    public Result loginEmail(String email, String senha) {
+               Usuario user = getUsuarioEmail(email);
+        if (user != null && user.getSenha().equals(senha)) {
+                       LoggedUserController.loggedUser = user;
+            return redirect(routes.LoggedUserController.index());
+                   } else {
+                       return badRequest("Usuario e/ou senha incorretos.");
+        }
+          }
+
+           public Result loginUsername(String username, String senha) {
+               Usuario user = getUsuarioUsername(username);
+               if (user != null && user.getSenha().equals(senha)) {
+                       LoggedUserController.loggedUser = user;
+                       return redirect(routes.LoggedUserController.index());
+                   } else {
+                       return badRequest("Usuario e/ou senha incorretos.");
+                   }
+           }
+
+    private Usuario getUsuarioUsername(String username){
+                for(Usuario usuario:listaUsuarios){
+                       if(usuario.getUsername().equals(username)){
+                                return usuario;
+                            }
+
+                            }
+               return null;
+           }
+
+
 }
